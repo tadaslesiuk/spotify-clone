c07d117451a862a2521b6a37d345b4fdd547eb8f
tadaslesiuk <tadaslesiuk@gmail.com>
2022-01-12 15:01:13 +0200

    Implemented middleware for guarded routes

---
+++ pages/_middleware.js
@@ -0,0 +1,19 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export const middleware = async (req, res) => {
+    // JWT token will exist if the user is logged in
+    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+    const { pathname } = req.nextUrl;
+
+    // JWT token exists or request is made to auth API
+    if (token || pathname.includes('/api/auth')) {
+        return NextResponse.next();
+    }
+
+    // JWT token does not exist and request is made to a guarded route
+    if (!token && pathname !== '/login') {
+        return NextResponse.redirect('/login');
+    }
+};
