580b244a281cd8b1a9b2cae1afeae5de0e194efb
tadaslesiuk <tadaslesiuk@gmail.com>
2022-01-12 23:52:45 +0200

    Added track duration display

--- components/Song/Song.js
+++ components/Song/Song.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import useSpotify from '../../hooks/useSpotify';
+import { msToMinAndSec } from '../../lib/msToMinAndSec';
 
 const Song = ({ order, track }) => {
     const spotifyApi = useSpotify();
     const [artists, setArtists] = useState('');
+    console.log(track.track);
 
     useEffect(() => {
         const artistCount = track.track.artists.length;
@@ -16,8 +18,8 @@ const Song = ({ order, track }) => {
     }, []);
 
     return (
-        <div>
-            <div>
+        <div className="grid grid-cols-2">
+            <div className="flex items-center space-x-4">
                 <p>{order + 1}</p>
                 <img
                     className="w-10 h-10"
@@ -28,6 +30,10 @@ const Song = ({ order, track }) => {
                     <p>{track.track.name}</p>
                     <p>{artists}</p>
                 </div>
+            </div>
+            <div className="flex items-center justify-between ml-auto md:ml-0">
+                <p className="hidden md:inline">{track.track.album.name}</p>
+                <p>{msToMinAndSec(track.track.duration_ms)}</p>
             </div>
         </div>
     );
