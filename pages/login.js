a4cf6ac2f5eb8a172268bb9edf597f10082762bc
tadaslesiuk <tadaslesiuk@gmail.com>
2022-01-17 03:39:07 +0200

    Improved login page

--- pages/login.js
+++ pages/login.js
@@ -2,27 +2,26 @@ import { getProviders, signIn } from 'next-auth/react';
 
 const Login = ({ providers }) => {
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen bg-black'>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-black">
             <img
-                className='w-24 mb-10'
-                src='https://links.papareact.com/9xl'
-                alt=''
+                className="w-60 mb-2"
+                src="https://logos-world.net/wp-content/uploads/2020/09/Spotify-Logo.png"
+                alt=""
             />
             {Object.values(providers).map((p) => (
                 <div key={p.name}>
                     <button
-                        className='flex text-white border rounded-md p-3 hover:bg-gray-800'
+                        className="flex items-center text-black text-sm tracking-wider uppercase font-bold rounded-full h-12 px-10 bg-[#1ED760] hover:bg-opacity-90"
                         onClick={() => signIn(p.id, { callbackUrl: '/' })}
                     >
-                        <img
-                            className='w-6 mr-3'
-                            src='https://links.papareact.com/9xl'
-                            alt=''
-                        />
-                        Continue with {p.name}
+                        Sign in with {p.name}
                     </button>
                 </div>
             ))}
+            <p className="text-white/50 mt-8 w-80 text-center">
+                To sign in to this application, you will need to gain access
+                permission from the developer.
+            </p>
         </div>
     );
 };
